1d2280fad83fb4f93953eb9e639ea6e6ba30c2d9
hassan <h.souiti@gmail.com>
2019-12-23 09:57:00 +0100

    configuring ts

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+} from 'react-native';
+
+
+const App = () => {
+    return (
+        <>
+            <SafeAreaView>
+                <ScrollView>
+                    <Text>Hello</Text>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
